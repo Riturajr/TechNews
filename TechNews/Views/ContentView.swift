cedd5d3dc3fc578a3e0ad86a9d8050716a3a8727
Rituraj <riturajnambiar98@gmail.com>
2021-08-04 14:49:30 +0530

    Use WebView to display the content

--- TechNews/Views/ContentView.swift
+++ TechNews/Views/ContentView.swift
@@ -13,16 +13,19 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(networkManager.posts) { post in
-                HStack {
-                    Text(String(post.points))
-                    Text(post.title)
+                NavigationLink(
+                    destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
                 }
                 
             }
             .navigationTitle("TechNews")
         }
         .onAppear {
-        self.networkManager.fetchData()
+            self.networkManager.fetchData()
         }
     }
 }
@@ -35,9 +38,4 @@ struct ContentView_Previews: PreviewProvider {
 
 
 
-//let posts = [
-//    Post(id: "1" , title: "Hello"),
-//    Post(id: "2" , title: "Bonjour"),
-//    Post(id: "3" , title: "Hola")
-//]
 
